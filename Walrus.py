cce678360c9cdcbc8584f3764d9b7fdc9a9bc773
Aagya <143220692+Aagya-maker@users.noreply.github.com>
2023-12-07 18:52:27 +0545

    Add files via upload

---
+++ Walrus.py
@@ -0,0 +1,25 @@
+#Walrus Operator
+#Walrus uses := syntax
+#it assigns values to variables as part of a larger expression.
+
+
+print(a:=False) #output=False
+print(b:=8) #output=8
+print(aagya:="name") #Walrus uses := syntax  #it assigns values to variables as part of a larger expression.
+#examples
+numbers=[1,2,3,4,5]
+while (n:=len(numbers))>0:
+  print(numbers.pop())
+#output is 
+# 5
+# 4
+# 3
+# 2
+# 1
+
+
+
+print(Happy := True)
+#Output is True
+
+#Why is it called a Walrus? Well, look at it, if you tilt your head to the right, you will see a Walrus :=
